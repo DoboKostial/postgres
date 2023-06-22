f1145061bca5c6422ac8aad7ed658c03fd357dca
Ďobo Chajbullin Koštial <dobo@dobo.sk>
2023-06-22 13:23:02 +0200

    Create postgres_owned_objects.sql
    This sql script lists all objects owned by particular user.oll objects owned by particular user.

---
+++ diagnostic_scripts/postgres_owned_objects.sql
@@ -0,0 +1,25 @@
+########################################
+This sql script lists all objects owned by particular user.oll objects owned by particular user.
+Very usefull when deleting user and should know his objects...
+########################################
+
+SELECT                     
+    pg_namespace.nspname AS SchemaName,
+    pg_class.relname AS ObjectName, 
+    pg_roles.rolname AS ObjectOwner,
+    CASE pg_class.relkind
+        WHEN 'r' THEN 'TABLE'
+        WHEN 'm' THEN 'MATERIALIZED_VIEW'
+        WHEN 'i' THEN 'INDEX'
+        WHEN 'S' THEN 'SEQUENCE'
+        WHEN 'v' THEN 'VIEW'
+        WHEN 'c' THEN 'TYPE'
+        ELSE pg_class.relkind::text
+    END AS ObjectType
+FROM pg_class
+ JOIN pg_roles  ON pg_roles.oid = pg_class.relowner
+ JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
+WHERE pg_namespace.nspname NOT IN ('information_schema', 'pg_catalog')
+    AND pg_namespace.nspname NOT LIKE 'pg_toast%'
+    AND pg_roles.rolname = 'zmizik'  
+ORDER BY pg_namespace.nspname, pg_class.relname;
