4dc4d1f789a5065f5874bd2fef4e6129a2098bef
dobo <dobo@dobo.sk>
2024-07-31 21:23:07 +0200

    this script lists 10 largest tables in DB

---
+++ diagnostic_scripts/10_largest_table_size.sql
@@ -0,0 +1,9 @@
+SELECT                             
+    schemaname AS schema_name, 
+    relname AS table_name,
+    pg_size_pretty(pg_total_relation_size(relid)) AS total_table_size
+FROM pg_catalog.pg_statio_user_tables
+ORDER BY pg_total_relation_size(relid) DESC,
+         pg_relation_size(relid) DESC
+LIMIT 20;
+
