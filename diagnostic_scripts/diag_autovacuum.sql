c9beaedf268ac1578c25114c201244f8962fdbc0
dobo <dobo@dobo.sk>
2024-12-11 17:17:31 +0100

    added routine for custom table autovacuum settings

--- diagnostic_scripts/diag_autovacuum.sql
+++ diagnostic_scripts/diag_autovacuum.sql
@@ -1,3 +1,6 @@
+##
+#this lists all tables according dead tuples and appropriate info (when last autovacuum etc...)
+##
 SELECT relname  AS "table_name", 
        Extract (epoch FROM CURRENT_TIMESTAMP - last_autovacuum) AS since_last_autovac, 
        autovacuum_count,
@@ -9,3 +12,16 @@ SELECT relname  AS "table_name",
 FROM   pg_stat_all_tables 
 WHERE  schemaname = 'public'
 ORDER  BY n_dead_tup DESC; 
+
+##
+# this lists all custom autovacuum (=not cluster wide) parameters defined for table autovacuum
+##
+SELECT
+    relname AS table_name,
+    option_name,
+    option_value
+FROM
+    pg_class c
+    LEFT JOIN LATERAL pg_options_to_table(c.reloptions) opt ON true
+WHERE
+    relname = 'NAZEV_TABULKY';
